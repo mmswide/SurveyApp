39ee7dc4e600d9915b9b0712618f29b3f06a1d3d
favila <frankavila00@gmail.com>
2015-12-21 13:16:44 -0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+   before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+   before_action :correct_user,   only: [:edit, :update]
+   before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -8,15 +11,42 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+     @users = User.paginate(page: params[:page])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    flash[:success] = "Profile updated"
+    redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  #delete that dumb user!
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   private
@@ -24,5 +54,27 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+
+       # Before filters
+  end
+
+  # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end       
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+  end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
 
 def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "DanceFest"
   end
 
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "DanceFest"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | DanceFest"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	 assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  	 assert_select "title", "About | DanceFest"
   end
 
   test "should get contact" do
