2ec5b483552d7715a486b784273d56f1dd1a304b
favila <frankavila00@gmail.com>
2015-12-21 16:27:26 -0800

    Add account activations

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,14 +16,14 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
-
- # Returns the user corresponding to the remember token cookie.
-  def current_user
+  
+    # Returns the current logged-in user (if any).
+    def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+       if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -34,6 +34,7 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
   # Forgets a persistent session.
   def forget(user)
     user.forget
