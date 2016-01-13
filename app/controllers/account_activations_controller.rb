af3008cb4ffbf8fbc4a0a0e671bc4339bc5557e0
favila <frankavila00@gmail.com>
2016-01-13 14:47:10 -0800

    sending user to the event dashboard after account activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -6,7 +6,7 @@ class AccountActivationsController < ApplicationController
       user.activate
       log_in user
       flash[:success] = "Account activated!"
-      redirect_to user
+      redirect_to(:controller => 'events', :action => 'index')
     else
       flash[:danger] = "Invalid activation link"
       redirect_to root_url
