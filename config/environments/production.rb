36c1ef26975a6e0050a51b11e1f6a36be156f422
favila <frankavila00@gmail.com>
2016-01-14 15:17:59 -0800

    updating production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,10 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = true
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'https://fathomless-ocean-4021.herokuapp.com'
