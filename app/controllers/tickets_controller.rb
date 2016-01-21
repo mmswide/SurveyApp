a418d99fc9ddd5dcb01474c2c6f5c57703ad865d
favila <frankavila00@gmail.com>
2016-01-20 20:58:13 -0800

    finished updating routes and links to use new routing convention

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -20,10 +20,10 @@ class TicketsController < ApplicationController
 
   def create
     @ticket = Ticket.new(ticket_params)
-     @ticket.save
+    @ticket.save
     if @ticket.save
       flash[:success] = "Ticket created successfully!!"
-      redirect_to(:action =>'index', :event_id => @event.id)
+      redirect_to event_tickets_path
     else
       render('new')
     end
@@ -48,13 +48,13 @@ class TicketsController < ApplicationController
   def destroy
      ticket = Ticket.find(params[:id]).destroy
      flash[:success] = "Ticket '#{ticket.ticket_name}' destroyed"
-     redirect_to(:action => 'index', :event_id => @event.id)
+     redirect_to event_tickets_path
   end
 
   private
 
   def ticket_params
-    params.require(:ticket).permit(:ticket_name, :ticket_decription, :ticket_price)
+    params.require(:ticket).permit( :event_id, :ticket_name, :ticket_decription, :ticket_price)
   end 
 
   def find_event
