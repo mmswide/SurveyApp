c2ab3ae549e3fae15dd3e63f204785618ec7c439
Alexey S. Brovikov <alexey.brovikov@gmail.com>
2017-02-02 09:30:48 -0800

    Update gitingnore and schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111075336) do
+ActiveRecord::Schema.define(version: 20170126025848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 20170111075336) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
-    t.integer  "event_id"
   end
 
   create_table "events", force: :cascade do |t|
